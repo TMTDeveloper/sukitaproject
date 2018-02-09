648feee83f1c0ff123c3a03bdcf4cbc723a7211c
rinaldiabimanyu93 <rinaldiabimanyu@gmail.com>
2018-02-09 11:04:38 +0700

    4th commit

---
+++ application/views/netad/home_page/home_sidebar.php
@@ -0,0 +1,90 @@
+<nav id="sidebar" class="" style="
+    min-width: 250px;
+    max-width: 250px;
+    background: #ffffff;
+    color: #000;
+    transition: all 0.6s cubic-bezier(0.945, 0.020, 0.270, 0.665);
+    transform-origin: bottom left;
+">
+                <div class="sidebar-header">
+                    <img src=" http://localhost/sukita_project/assets/img/sukitap.png " alt="Sukita" href="#" style="
+    width: 200px;
+">
+                </div>
+
+                <ul class="list-unstyled components" style="
+    min-width: 250px;
+    max-width: 250px;
+    background: #ffffff;
+    color: #000;
+    transition: all 0.6s cubic-bezier(0.945, 0.020, 0.270, 0.665);
+    transform-origin: bottom left;
+    padding-top: 0px;
+">
+                    <li class="active">
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="true" class="" style="color: #000;
+    background: #ffffff;">Terkini</a>
+                        <ul class="list-unstyled collapse in" id="homeSubmenu" aria-expanded="true" style="">
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Ada Aja</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Hari Ini</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Hubungan</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Kuliner</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Motivasi</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Tips</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Travel</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Tutorial</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Review</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Film</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Pojok Komik</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Mistis</a></li>
+                            <li><a href="#" style="font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #fafafa">Story</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Terpopuler</a>
+                        <!-- <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">Pages</a>
+                        <ul class="collapse list-unstyled" id="pageSubmenu">
+                            <li><a href="#">Page 1</a></li>
+                            <li><a href="#">Page 2</a></li>
+                            <li><a href="#">Page 3</a></li>
+                        </ul> -->
+                    </li>
+                    <li>
+                        <a href="#">Terbaru</a>
+                    </li>
+                    <li>
+                        <a href="#">Event</a>
+                    </li>
+                </ul>
+
+                <ul class="list-unstyled CTAs">
+                    <li><a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a></li>
+                    <li><a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a></li>
+                </ul>
+            </nav>
